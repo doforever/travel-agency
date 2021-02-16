4d683fb26c94a2296612daa4044b379fcb108c12
Dorota Tomala <doforever@gmail.com>
2021-02-16 14:16:08 +0100

    Pass props to OrderOption's subcomponents

---
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const OrderOptionIcons = ({name}) => {
+  return (
+    <div>{name}</div>
+  );
+};
+
+OrderOptionIcons.propTypes = {
+  name: PropTypes.string,
+};
+
+export default OrderOptionIcons;
---
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const OrderOptionNumber = ({name}) => {
+  return (
+    <div>{name}</div>
+  );
+};
+
+OrderOptionNumber.propTypes = {
+  name: PropTypes.string,
+};
+
+export default OrderOptionNumber;
