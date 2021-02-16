7cbcf4bbb6d1a995f6b124839ab94c43705d0ca7
Dorota Tomala <doforever@gmail.com>
2021-02-16 15:27:45 +0100

    Allow to change trip options

--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {formatPrice} from '../../../utils/formatPrice';
 
-const OrderOptionIcons = ({name}) => {
-  return (
-    <div>{name}</div>
-  );
-};
+import Icon from '../../common/Icon/Icon';
+
+import styles from './OrderOption.scss';
+
+const OrderOptionIcons = ({values, required, setOptionValue}) => (
+  <div className={styles.dropdown}>
+    {required ? '' : (
+      <div onClick={() => setOptionValue('')}><Icon name='time-circle'/> none</div>
+    )}
+    {values.map(value => (
+      <div key={value.id} onClick={() => setOptionValue(value.id)} className={styles.icon}>
+        <Icon name={value.icon}/> {value.name} ({formatPrice(value.price)})
+      </div>
+    ))}
+  </div>
+);
 
 OrderOptionIcons.propTypes = {
-  name: PropTypes.string,
+  values: PropTypes.array,
+  required: PropTypes.bool,
+  setOptionValue: PropTypes.func,
 };
 
 export default OrderOptionIcons;
--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {formatPrice} from '../../../utils/formatPrice';
 
-const OrderOptionNumber = ({name}) => {
+import styles from './OrderOption.scss';
+
+const OrderOptionNumber = ({price, setOptionValue, currentValue, limits}) => {
   return (
-    <div>{name}</div>
+    <div className={styles.number}>
+      <input
+        type='number'
+        className={styles.inputSmall}
+        value={currentValue}
+        min={limits.min}
+        max={limits.max}
+        onChange={event => setOptionValue(parseInt(event.currentTarget.value))}/>
+      ({formatPrice(price)})
+    </div>
   );
 };
 
 OrderOptionNumber.propTypes = {
-  name: PropTypes.string,
+  price: PropTypes.string,
+  setOptionValue: PropTypes.func,
+  currentValue: PropTypes.number,
+  limits: PropTypes.objectOf(PropTypes.number),
 };
 
 export default OrderOptionNumber;
