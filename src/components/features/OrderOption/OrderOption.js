2c6d8407ab90f76271e16d7dba87e3a712ce3c82
Dorota Tomala <doforever@gmail.com>
2021-02-17 10:52:13 +0100

    Add OrderOptionText

--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -5,6 +5,7 @@ import OrderOptionDropdown from './OrderOptionDropdown';
 import OrderOptionIcons from './OrderOptionIcons';
 import OrderOptionNumber from './OrderOptionNumber';
 import OrderOptionCheckboxes from './OrderOptionCheckboxes';
+import OrderOptionText from './OrderOptionText';
 
 import styles from './OrderOption.scss';
 
@@ -13,6 +14,7 @@ const optionTypes = {
   icons: OrderOptionIcons,
   checkboxes: OrderOptionCheckboxes,
   number: OrderOptionNumber,
+  text: OrderOptionText,
 };
 
 const OrderOption = ({name, type, id, setOrderOption, ...otherProps}) => {
