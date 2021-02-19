f4df56a10463025dbd57db1cbc4064250dcf50b0
Dorota Tomala <doforever@gmail.com>
2021-02-19 14:36:13 +0100

    Send incomplete order to server

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
 
 import pricing from '../../../data/pricing.json';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from './../../common/Button/Button';
 
 import {Row, Col} from 'react-flexbox-grid';
 import styles from './OrderForm.scss';
+
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 const OrderForm = ({tripCost, days, options, setOrderOption}) => {
   return (
@@ -19,6 +50,7 @@ const OrderForm = ({tripCost, days, options, setOrderOption}) => {
       <Col xs={12} className={styles.column}>
         <OrderSummary cost={tripCost} days={days} options={options}/>
       </Col>
+      <Button onClick={() => sendOrder(options, tripCost)}>Order now!</Button>
     </Row>
   );
 };
